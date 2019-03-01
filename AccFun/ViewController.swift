5d9058b3ece16e1399bb9e3905fffae093c0b22b
pastramafoo <mpoelsterl1217@gmail.com>
2019-02-28 20:27:22 -0600

    Initial commit

--- AccFun/ViewController.swift
+++ AccFun/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    @IBOutlet weak var accelTagX: UILabel!
+    @IBOutlet weak var accelTagY: UILabel!
+    @IBOutlet weak var accelTagZ: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    let motionManager = CMMotionManager()
+    func startAccelerometer () {
+        motionManager.accelerometerUpdateInterval = 1
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!, withHandler: {
+            (accelerData:CMAccelerometerData?, error: Error?) in
+            if (error != nil ) {
+                print("Error")
+            } else {
+                
+                let accelX = accelerData?.acceleration.x
+                self.accelTagX.text = String(format: "%.02f", accelX!)
+                let accelY = accelerData?.acceleration.y
+                self.accelTagY.text = String(format: "%.02f", accelY!)
+                let accelZ = accelerData?.acceleration.z
+                self.accelTagZ.text = String(format: "%.02f", accelZ!)
+                print("Accelerometer X: \(accelX!)")
+            }
+        })
+    }
+    func stopAccelerometer () {
+        self.motionManager.stopAccelerometerUpdates()
+        self.accelTagX.text = "--"
+        self.accelTagY.text = "--"
+        self.accelTagZ.text = "--"
+        print("Accelerometer X Stopped")
+    }
+    
+    //MARK: Actions
+    @IBAction func stateChange(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            startAccelerometer()
+        } else {
+            stopAccelerometer()
+        }
+    }
 
 }
 
